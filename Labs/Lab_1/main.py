959cda7e3c46589ec5cee45c6bf031f669776e6e
Ilina Ksenia <114830177+KsushaI@users.noreply.github.com>
2023-12-26 00:05:24 +0300

    Create main.py

---
+++ Labs/Lab_1/main.py
@@ -0,0 +1,21 @@
+from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.square import Square
+from lab_python_oop.circle import Circle
+from art import *
+
+
+rectangle1 = Rectangle(10,10, "синего")
+print(rectangle1)
+for i in range(5):
+  aprint("coffee", number=5)
+circle1 = Circle(10, "зелёного")
+print(circle1)
+print('     ', end='')
+aprint("coffee", number=1,)
+aprint("coffee", number=3)
+print('     ', end='')
+aprint("coffee", number=1)
+square1 = Square(5, "красного")
+print(square1)
+for i in range(7):
+  aprint("coffee", number=5)
